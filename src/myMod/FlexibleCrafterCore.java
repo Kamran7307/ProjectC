cc7220a93db49028c8d37de3f9f36e99039b0ba4
Kamran7307 <kamranreazon@gmail.com>
2025-06-11 09:31:39 +0530

    Update FlexibleCrafterCore.java

--- src/myMod/FlexibleCrafterCore.java
+++ src/myMod/FlexibleCrafterCore.java
@@ -1,1 +1,10 @@
+package mymod;
 
+import mindustry.mod.Mod;
+import arc.util.Log;
+
+public class FlexibleCrafterCore extends Mod {
+    public FlexibleCrafterCore() {
+        Log.info("FlexibleCrafterCore loaded.");
+    }
+}
